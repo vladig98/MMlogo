d31f1267b9ad90468245ca1854197aceecf551d9
Vladimir Gotsin <32268856+vladig98@users.noreply.github.com>
2020-02-17 14:42:56 +0200

    Final tests

--- MMlog.cs
+++ MMlog.cs
@@ -11,20 +11,22 @@ namespace MMLogo
             int j = 0;
             int limit = (int)Math.Ceiling(n / 2.0);
             int k = 1;
+            int l = 0;
 
             for (int i = n; i >= 0; i--)
             {
                 string dashBeginning = new string('-', i);
                 string starsBeginning = i < limit ? new string('*', n) : new string('*', n + j);
                 string dashMiddle = i < limit ? null : new string('-', n - j);
-                string starsBetweenStars = i < limit ? new string('*', i + j + 2) : null;
+                string starsBetweenStars = i < limit ? new string('*', n + j - l) : null;
                 string dashMiddleMiddle = new string('-', i * 2);
                 string dashesBetweenStars = i < limit ? new string('-', k) : null;
                 k = i < limit ? k + 2 : k;
                 string beginning = dashBeginning + starsBeginning + dashesBetweenStars + starsBetweenStars + dashesBetweenStars + dashMiddle + starsBeginning;
                 string end = string.Join("", beginning.ToCharArray().Reverse());
                 Console.WriteLine(beginning + dashMiddleMiddle + end);
-                j = i < limit ? j - 1 : j + 2;
+                j = i <= limit ? j : j + 2;
+                l = i < limit ? l + 2 : l;
             }
         }
     }
